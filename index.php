a2e2635f551965023e4cb404b5a82414e1ccf2a0
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-09-06 14:51:31 +0700

    page home

--- index.php
+++ index.php
@@ -1,5 +1,62 @@
 <?php $data["title"] = "Trang chủ"; ?>
-<?php $is_home = true; ?>
 <?php require "template-parts/layouts/header.php"; ?>
-<div uk-height-viewport="offset-top: true,offset-bottom:true"></div>
+<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="ratio: false;animation: scale; autoplay: true;">
+
+    <ul class="uk-slideshow-items" uk-height-viewport="offset-top: true; offset-bottom:0">
+        <?php
+        $data = array(
+            array(
+                'src' => 'images/Facade1.png',
+                'title1' => 'Ecopark Swanlake The Onsen',
+                'title2' => 'Tổ hợp nghỉ dưỡng khoáng nóng <br class="uk-visible@m"> giữa lòng Ecopark',
+            ),
+            array(
+                'src' => 'images/image3.png',
+                'title1' => 'Ecopark Swanlake The Landmark',
+                'title2' => 'Khoáng nóng tại gia, <br class="uk-visible@m"> Trọn đời trị liệu',
+            ),
+            array(
+                'src' => 'images/image2.png',
+                'title1' => 'Ecopark Grand The Island <br class="uk-visible@m"> Sofitel Residences Villas',
+                'title2' => 'Đẳng cấp thượng lưu thực sự',
+            ),
+            array(
+                'src' => 'images/15706149972575111.png',
+                'title1' => 'Malibu Residence Hội An',
+                'title2' => 'Tuyệt tác giữa lòng di sản',
+            ),
+        );
+        foreach ($data as $k => $v): ?>
+            <li>
+                <div class="uk-position-cover uk-animation-kenburns uk-animation-reverse uk-transform-origin-bottom-right">
+                    <img src="<?= $v['src'] ?>" alt="" uk-cover>
+                </div>
+                <div class="uk-position-cover home__slide__overlay">
+
+                </div>
+                <div class="uk-position-cover uk-section-small home__slide__section uk-flex uk-flex-column uk-flex-between">
+                    <div>
+                        <div class="uk-container" uk-scrollspy="target: > *; cls: uk-animation-slide-top; delay: 500; repeat: true">
+                            <h3 class="uk-h3 home__slide__section__title1">LEVISTA VIỆT NAM TRÂN TRỌNG GIỚI THIỆU</h3>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-container" uk-scrollspy="target: > *; cls: uk-animation-slide-bottom; delay: 500; repeat: true">
+                            <div class="home__slide__section__title2"><?= $v['title1'] ?></div>
+                            <h2 class="uk-h2 home__slide__section__title3"><?= $v['title2'] ?></h2>
+                            <a href="" class="home__slide__section__btn uk-button uk-button-default uk-button-small uk-border-pill">Tìm hiểu thêm</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+
+    <ul class="uk-slideshow-nav uk-position-bottom-center uk-dotnav uk-flex-center uk-margin"></ul>
+
+</div>
+
 <?php require "template-parts/layouts/footer.php"; ?>
--- template-parts/layouts/header.php
+++ template-parts/layouts/header.php
@@ -18,40 +18,71 @@
     <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit-icons.min.js"></script>
     <script src="js/app.js"></script>
 </head>
-<body class="<?= ($is_home) ? 'isHome' : '' ?>">
+<body class="">
 <?php require "template-parts/commons/facebook_chat.php"; ?>
 <div id="app" class="uk-height-viewport uk-offcanvas-content uk-overflow-hidden uk-position-relative">
-<div id="header" class="header">
-    <div class="uk-container uk-padding-remove">
-        <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+<div id="offcanvas-flip" uk-offcanvas="flip: true; overlay: true">
+    <div class="uk-offcanvas-bar">
 
-            <div class="uk-navbar-left">
-                <a href="" class="uk-navbar-item uk-logo"><img src="images/logo1.png" alt=""></a>
+        <button class="uk-offcanvas-close" type="button" uk-close></button>
+
+        <div class="uk-display-inline-block header__lang">
+            <div class="uk-position-center" style="top: calc(50% - 3px)">
+                <a href="" class="uk-active">VN</a> <span>/</span> <a href="">EN</a>
             </div>
+        </div>
 
-            <div class="uk-navbar-right">
+        <ul class="uk-nav-default uk-nav-parent-icon uk-margin header__menuNav" uk-nav>
+            <li class="uk-active"><a href="#">TRANG CHỦ</a></li>
+            <li><a href="#">GIỚI THIỆU</a></li>
+            <li><a href="#">DỰ ÁN</a></li>
+            <li><a href="#">Privilege</a></li>
+            <li><a href="#">TIN TỨC</a></li>
+            <li><a href="#">Cơ hội nghề nghiệp</a></li>
+            <li><a href="#">Liên hệ</a></li>
+        </ul>
 
-                <div>
-                    <div class="uk-text-right uk-visible@m">
-                        <div class="uk-display-inline-block header__lang">
-                            <div class="uk-position-center" style="top: calc(50% - 3px)">
-                                <a href="" class="uk-active">VN</a> <span>/</span> <a href="">EN</a>
-                            </div>
-                        </div>
-                    </div>
-                    <ul class="uk-navbar-nav uk-visible@m">
-                        <li class="uk-active"><a href="#">TRANG CHỦ</a></li>
-                        <li><a href="#">GIỚI THIỆU</a></li>
-                        <li><a href="#">DỰ ÁN</a></li>
-                        <li><a href="#">Privilege</a></li>
-                        <li><a href="#">TIN TỨC</a></li>
-                        <li><a href="#">Cơ hội nghề nghiệp</a></li>
-                        <li><a href="#">Liên hệ</a></li>
-                    </ul>
+    </div>
+</div>
+<div uk-sticky="">
+    <div id="header" class="header">
+        <div class="uk-container uk-padding-remove">
+            <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+
+                <div class="uk-navbar-left">
+                    <a href="" class="uk-navbar-item uk-logo"><img src="images/logo1.png" alt=""></a>
                 </div>
 
-            </div>
+                <div class="uk-navbar-right">
+                    <div class="uk-navbar-item uk-hidden@m">
+                        <div id="m_nav_menu" class="m_nav menu" uk-toggle="target: #offcanvas-flip">
+                            <div class="m_nav_ham button_closed" id="m_ham_1"></div>
+                            <div class="m_nav_ham button_closed" id="m_ham_2"></div>
+                            <div class="m_nav_ham button_closed" id="m_ham_3"></div>
+                        </div>
+                    </div>
+                    <div class="uk-visible@m">
+                        <div class="uk-text-right">
+                            <div class="uk-display-inline-block header__lang">
+                                <div class="uk-position-center" style="top: calc(50% - 3px)">
+                                    <a href="" class="uk-active">VN</a> <span>/</span> <a href="">EN</a>
+                                </div>
+                            </div>
+                        </div>
+                        <ul class="uk-navbar-nav">
+                            <li class="uk-active"><a href="#">TRANG CHỦ</a></li>
+                            <li><a href="#">GIỚI THIỆU</a></li>
+                            <li><a href="#">DỰ ÁN</a></li>
+                            <li><a href="#">Privilege</a></li>
+                            <li><a href="#">TIN TỨC</a></li>
+                            <li><a href="#">Cơ hội nghề nghiệp</a></li>
+                            <li><a href="#">Liên hệ</a></li>
+                        </ul>
+                    </div>
 
-        </nav>
+                </div>
+
+            </nav>
+        </div>
     </div>
 </div>
