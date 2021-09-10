6e0f4c48098fa43b89445fd9162b45b6e7e0920f
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-09-11 00:29:06 +0700

    about

--- gioithieu.php
+++ gioithieu.php
@@ -3,11 +3,12 @@
 <?php require "template-parts/layouts/header.php"; ?>
 <div>
     <div id="fullpage">
+        <!--Gioi thieu chung-->
         <div class="section">
             <div class="uk-container uk-container-expand-right about__containerExpandRight uk-height-1-1">
                 <div class="uk-grid-match uk-height-1-1 uk-grid-96-m" uk-grid>
                     <div class="uk-width-expand">
-                        <div class="uk-flex-middle uk-flex">
+                        <div class="uk-flex-middle uk-flex about__thongtin__relative">
                             <div>
                                 <div class="uk-margin">
                                     <div class="uk-grid-small uk-child-width-auto" uk-grid>
@@ -36,6 +37,9 @@
                 </div>
             </div>
         </div>
+        <!--/Gioi thieu chung-->
+
+        <!--Tam nhin - su menh-->
         <div class="section">
             <div class="uk-flex uk-flex-column uk-height-1-1">
                 <div class="uk-flex-auto uk-flex uk-flex-middle uk-flex-center">
@@ -83,7 +87,144 @@
                 </div>
             </div>
         </div>
+        <!--/Tam nhin - su menh-->
+
+        <!--Gia tri cot loi-->
+        <div class="section">
+            <div class="uk-container uk-container-expand-right about__containerExpandRight uk-height-1-1">
+                <div class="uk-grid-match uk-height-1-1 uk-grid-96-m" uk-grid>
+                    <div class="uk-width-expand">
+                        <div class="uk-flex-middle uk-flex">
+                            <div>
+                                <div class="uk-margin">
+                                    <div class="uk-grid-small uk-child-width-auto" uk-grid>
+                                        <div>
+                                            <img class="tuyendung__box1__img" src="images/Group2.png" alt="">
+                                        </div>
+                                        <div class="uk-width-expand">
+                                            <div class="tuyendung__box1__title uk-margin-medium">
+                                                <div>Giá Trị</div>
+                                                Cốt Lõi
+                                            </div>
+                                            <div class="uk-flex-right" uk-grid>
+                                                <div class="uk-width-3-5@m">
+                                                    <div class="about__giatri__item">
+                                                        <div class="uk-grid-10" uk-grid>
+                                                            <div class="uk-width-auto">
+                                                                <img class="tuyendung__content__img" src="images/tuyendung/Vector.png" alt="">
+                                                            </div>
+                                                            <div class="uk-width-expand">
+                                                                <h3 class="uk-h3 about__giatri__title">TÂM</h3>
+                                                                <div class="about__thongtin__txt2">Tận tâm đồng hành cùng khách hàng trên mọi khía cạnh sử dụng sản phẩm - dịch vụ.</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="about__giatri__item">
+                                                        <div class="uk-grid-10" uk-grid>
+                                                            <div class="uk-width-auto">
+                                                                <img class="tuyendung__content__img" src="images/tuyendung/Vector.png" alt="">
+                                                            </div>
+                                                            <div class="uk-width-expand">
+                                                                <h3 class="uk-h3 about__giatri__title">TRÍ</h3>
+                                                                <div class="about__thongtin__txt2">Kinh nghiệm của các chuyên gia trong lĩnh vực bất động sản kết hợp với các giải pháp nhạy bén đúc kết trong mỗi giải pháp cho từng nhu cầu của khách hàng.</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="about__giatri__item">
+                                                        <div class="uk-grid-10" uk-grid>
+                                                            <div class="uk-width-auto">
+                                                                <img class="tuyendung__content__img" src="images/tuyendung/Vector.png" alt="">
+                                                            </div>
+                                                            <div class="uk-width-expand">
+                                                                <h3 class="uk-h3 about__giatri__title">TRƯỜNG</h3>
+                                                                <div class="about__thongtin__txt2">Bằng một tâm nguyện kiên định, Levista hướng đến những giá trị phát triển bền vững, trường tồn và tốt hơn mỗi ngày.</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-width-1-2@m">
+                        <img class="uk-responsive-width uk-responsive-height uk-visible@m" src="images/about/Group330.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/Gia tri cot loi-->
+
+        <!--Lí Do Tin Tưởng-->
+        <div class="section">
+            <div class="uk-section-small">
+                <div class="uk-container">
+                    <div class="uk-margin-large">
+                        <div class="uk-grid-small uk-child-width-auto" uk-grid>
+                            <div>
+                                <img class="tuyendung__box1__img" src="images/Group2.png" alt="">
+                            </div>
+                            <div class="uk-width-expand">
+                                <div class="tuyendung__box1__title uk-margin-medium">
+                                    <div>Lí Do</div>
+                                    Tin Tưởng
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-child-width-1-2 uk-child-width-1-3@m about__lido__grid uk-flex-center uk-grid-match uk-grid-small uk-grid-20-m" uk-grid>
+                        <div>
+                            <div class="about__lido__item">
+                                <span>01.</span>
+                                <div class="duan__chinhsach__desc"><span>Chuyên nghiệp:</span> Đội ngũ nhân viên Levista được đào tạo bởi những chuyên gia hàng đầu và có bằng chứng nhận chuyên nghiệp.</div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="about__lido__item">
+                                <span>02.</span>
+                                <div class="duan__chinhsach__desc"><span>Am hiểu và nhạy bén</span> với thị trường Bất động sản trong nước và quốc tế.</div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="about__lido__item">
+                                <span>03.</span>
+                                <div class="duan__chinhsach__desc"><span>Quan hệ chặt chẽ</span> với các chủ đầu tư uy tín.</div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="about__lido__item">
+                                <span>04.</span>
+                                <div class="duan__chinhsach__desc"><span>Nghiên cứu</span> kỹ lưỡng, khắt khe <span>tuyển chọn</span> đối với từng sản phẩm.</div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="about__lido__item">
+                                <span>05.</span>
+                                <div class="duan__chinhsach__desc"><span>Đồng hành</span> cùng phát triển.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/Lí Do Tin Tưởng-->
+
+        <!--Triết lý Kinh doanh-->
+        <div class="section">
+            
+        </div>
+        <!--/Triết lý Kinh doanh-->
+
+        <!---->
         <div class="section">Some section</div>
+        <!--/-->
+
+        <!---->
+        <div class="section">Some section</div>
+        <!--/-->
+
         <div class="section">
             <div>
                 <?php require "template-parts/layouts/footer1.php"; ?>
@@ -94,13 +235,24 @@
 
 <script>
     new fullpage('#fullpage', {
-        //options here
+
+        //Navigation
+
+
+        //Scrolling
         autoScrolling:true,
         scrollHorizontally: true,
-        loopBottom: true,
+        loopBottom: false,
+        fitToSection: true,
+
+        //Accessibility
+        keyboardScrolling: true,
 
         //Design
-        paddingTop: '93px',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+
+
 
     });
 
